f5b7ecb18563ecbe44eda30881fa827b9cc9494a
ck1049 <18238489029@163.com>
2023-02-20 00:04:28 +0800

    配置代理，解决跨域问题

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://www.loafer.online/lottery",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  }
 })
