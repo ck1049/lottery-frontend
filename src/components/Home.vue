3475cec2cd2f751c71bd51a7b585e8b830f4f5bf
ck1049 <18238489029@163.com>
2023-02-24 00:14:47 +0800

    增加导航功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,70 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { Document, Menu as IconMenu, Location, Setting, } from '@element-plus/icons-vue'
 
 const route = useRoute();
 const router = useRouter();
 
-const toLotto = () => {
-    console.log(router);
-    router.push("/lotto/1");
-};
-const toTwoColorBall = () => {
-    router.push("/twoColorBall/1");
-};
+const defaultActive = ref("/lotto/1");
+onMounted(() => {
+    router.push(defaultActive.value);
+});
+
+const handleOpen = (key, keyPath) => {
+    console.log(key, keyPath)
+}
+const handleClose = (key, keyPath) => {
+    console.log(key, keyPath)
+}
+const handleSizeChange = (size) => {
+
+}
 </script>
 <template>
-    <el-row>
-        <el-col :span="2" :offset="10">
-            <el-link type="primary" href="javascript:void(0)" @click="toLotto">大乐透</el-link>
-        </el-col>
-        <el-col :span="2">
-            <el-link type="primary" href="javascript:void(0)" @click="toTwoColorBall">双色球</el-link>
-        </el-col>
-    </el-row>
-    <router-view></router-view>
+    <div class="common-layout">
+        <el-container>
+            <el-header>
+
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <el-menu v-model:default-active="defaultActive" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :router="true">
+                        <el-sub-menu index="1">
+                            <template #title>
+                                <!-- <el-icon>
+                                    <location />
+                                </el-icon> -->
+                                <span>体彩大乐透</span>
+                            </template>
+                            <el-menu-item index="/lotto/1">开奖信息</el-menu-item>
+                            <el-menu-item index="1-2">实用工具</el-menu-item>
+                        </el-sub-menu>
+                        <el-sub-menu index="2">
+                            <template #title>
+                                <!-- <el-icon>
+                                    <location />
+                                </el-icon> -->
+                                <span>福彩双色球</span>
+                            </template>
+                            <el-menu-item index="/twoColorBalls/1">开奖信息</el-menu-item>
+                            <el-menu-item index="2-2">实用工具</el-menu-item>
+                        </el-sub-menu>
+                        <el-menu-item index="4">
+                            <el-icon>
+                                <setting />
+                            </el-icon>
+                            <span>settings</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
+  
 <style scoped></style>
+  
