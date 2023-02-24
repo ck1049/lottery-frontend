3921da50d2ecb76f5426ec5a77a93e3ca03d6da4
ck1049 <18238489029@163.com>
2023-02-25 01:27:14 +0800

    解决通过传参懒加载组件时无效的bug

--- src/tools/router copy.js
+++ src/tools/router copy.js
@@ -1,31 +1,26 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-import HelloWorld from '@/components/HelloWorld.vue'
-import Home from '@/components/Home.vue'
-import Lotto from '@/components/Lotto.vue'
-import TwoColorBall from '@/components/TwoColorBall.vue'
-
 const impComp = value => import(`@/components/${value}.vue`);
 const routes = [
     {
         name: "HelloWorld",
         path: "/HelloWorld",
-        component: HelloWorld
+        component: impComp('HelloWorld')
     },
     {
         name: "Home",
         path: "/home",
-        component: Home
+        component: impComp('Home')
     },
     {
         name: "Lotto",
         path: "/lotto/:index(\\d*)",
-        component: Lotto
+        component: impComp('Lotto')
     },
     {
         name: "TwoColorBall",
         path: "/twoColorBall/:index(\\d*)",
-        component: TwoColorBall
+        component: impComp('TwoColorBall')
     }
 ]
 const Router = createRouter({
